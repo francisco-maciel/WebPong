cc06a1e52fc0d62a79a1616dfa42614781e77214
Francisco Maciel <francisco.ma.maciel@gmail.com>
2015-08-26 13:33:34 +0100

    add gulp to run and watch

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('build', function () {
+    // TODO add building
+});
+
+gulp.task('test', function () {
+    // TODO add testing
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/*.*', []);
+  gulp.start('run');
+});
+
+gulp.task('run', ['build'], function () {
+    nodemon({
+        delay: 10,
+        script: './app.js',
+        cwd: "./src/",
+        args: ["config.json"],
+        ext: 'html js css'
+    })
+    .on('restart', function () {
+        util.log('server restarted!');
+    });
+});
