3fc69d591051a2ffe52ac1e056c51161becc2a2d
Francisco Maciel <francisco.ma.maciel@gmail.com>
2016-01-29 19:01:22 +0000

    Add socket.io

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 var http = require('http');
-var restify = require('restify');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var exec = require('child_process').exec;
+var util = require('util');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+//app.use(express.json());
+//app.use(express.urlencoded());
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
@@ -26,6 +31,10 @@ app.get('/mobile', function(req, res, next) {
 app.get('/hello/:name', respond);
 app.head('/hello/:name', respond);
 
-var listener = app.listen(1337, function() {
-  console.log('Listening at %s', JSON.stringify(listener.address().port));
+server.listen(1337, function() {
+    console.log('Listening at port %s',1337)
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
